24c8b398d32e8fdc6c448d199e054e1a45d74961
RST98\Julius Fiedler <ichderjulius@gmx.de>
2022-05-17 16:11:06 +0200

    new ut hash

--- ackrep_core/test/test_core.py
+++ ackrep_core/test/test_core.py
@@ -46,7 +46,7 @@ os.environ["ACKREP_DATABASE_PATH"] = os.path.join(core.root_path, "ackrep_core",
 os.environ["NO_IPS_EXCEPTHOOK"] = "True"
 
 # use `git log -1` to display the full hash
-default_repo_head_hash = "36d5e892fc7cf4a22a08362045cdc72edcd1e586"  # 2022-05-04 branch for_unittests
+default_repo_head_hash = "a7bbce29fd718b1a9e8f823a6ffac15a885fe38f"  # 2022-05-17 branch for_unittests
 
 
 class TestCases1(DjangoTestCase):
