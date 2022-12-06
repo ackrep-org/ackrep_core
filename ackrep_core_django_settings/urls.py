33147e4d4346a2c13c7f519ad61e508d99e801e5
Carsten Knoll <carsten.knoll@tu-dresden.de>
2022-12-06 23:30:36 +0100

    urls.py: remove calls to `url(...)` (not supported by django >=4.0)

--- ackrep_core_django_settings/urls.py
+++ ackrep_core_django_settings/urls.py
@@ -15,14 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
 
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    url(r"", include("ackrep_web.urls")),
+    path(r"", include("ackrep_web.urls")),
 ]
 
 if settings.DEBUG:
